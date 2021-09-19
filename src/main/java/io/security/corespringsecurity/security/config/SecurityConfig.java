526c7c54c973e832f4de28c5a8a25ae7de8d52c2
anpopo <dkstpgud@gmail.com>
2021-09-19 22:03:57 +0900

    정적 자원 관리 - WebIgnore 설정

--- src/main/java/io/security/corespringsecurity/security/config/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package io.security.corespringsecurity.security.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.boot.autoconfigure.security.servlet.StaticResourceRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -19,8 +22,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         String password = passwordEncoder().encode("12341234");
 
         auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
-        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
-        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER", "USER");
+        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN", "MANAGER", "USER");
     }
 
     @Bean
@@ -40,6 +43,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .and()
                 .formLogin();
+    }
 
+    // WebIgnore 설정 = js / css / images 등 필터를 적용할 필요가 없는 리소스 설정
+    // ignoring() 은 보안필터를 거치지 않음
+    // permitAll() 은 보안 필터를 거쳐 패턴 매칭
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
